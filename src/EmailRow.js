5b68c2f9684e44d2474e50eab204bf604f0ae31a
amotiv <89550131+amotiv@users.noreply.github.com>
2021-11-11 12:20:40 -0600

    Update EmailRow.js
    Added Attachment in email row

--- src/EmailRow.js
+++ src/EmailRow.js
@@ -13,7 +13,53 @@ function EmailRow({ id, title, subject, description, time }) {
       selectMail({
         id,
         title,
+        subject,import React from "react";
+
+import "./EmailRow.css";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { selectMail } from "./features/mailSlice";
+
+function EmailRow({ id, title, subject, description, time,avatar }) {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const openMail = () => {
+    dispatch(
+      selectMail({
+        id,
+        title,
         subject,
+        description,
+        time,
+        avatar,
+      })
+    );
+
+    history.push("/mail");
+  };
+
+  return (
+    <div onClick={openMail} className="emailRow">
+      <div className="emailRow__options">
+      </div>
+
+      <h3 className="emailRow__title">{title}</h3>
+
+      <div className="emailRow__message">
+        <h4>
+          {subject}{" "}
+          <span className="emailRow__description">- {description}</span>
+        </h4>
+      </div>
+
+      <p className="emailRow__time">{time}</p>
+      <img width="30" src={avatar} />
+    </div>
+  );
+}
+
+export default EmailRow;
         description,
         time,
       })
