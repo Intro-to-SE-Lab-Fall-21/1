5ec831ac84455c5efbd3f0d3f12c45a67cff724a
Cooper Riley <89549905+cooper350@users.noreply.github.com>
2021-11-18 09:12:57 -0600

    Update Mail.js

--- src/Mail.js
+++ src/Mail.js
@@ -6,10 +6,44 @@ import { IconButton } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import { selectOpenMail } from "./features/mailSlice";
 import { useSelector } from "react-redux";
+import { Button } from "@material-ui/core";
+import SidebarOption from "./SidebarOption";
+import { ArrowForward, Forward, Forward5, RepeatRounded, SendSharp } from "@material-ui/icons";
+import { useDispatch } from "react-redux";
+import { openForward } from "./features/mailSlice";
+import { useForm } from "react-hook-form";
+import { db } from "./firebase";
+import firebase from "firebase";
+
 
 function Mail() {
   const history = useHistory();
   const selectedMail = useSelector(selectOpenMail);
+  const dispatch = useDispatch();
+  const [fileUrl, setFileUrl] = React.useState(null)
+  const onFileChange = async (formData) =>{
+    const file = formData.target.files[0]
+    const storageRef = firebase.storage().ref()
+    const fileRef= storageRef.child(file.name)
+    await fileRef.put(file)
+    setFileUrl(await fileRef.getDownloadURL())
+
+  }
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  const onSubmit = (formData) => {
+    console.log(formData);
+    db.collection("emails").add({
+      to: formData.to,
+      subject: formData.subject,
+      message: formData.message,
+      avatar: fileUrl,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+
+   
+  };
 
   return (
     <div className="mail">
@@ -18,9 +52,34 @@ function Mail() {
           <IconButton onClick={() => history.push("/")}>
             <ArrowBackIcon />
           </IconButton>
+          
         </div>
+        
 
       </div>
+      <div className="forward">
+      <Button
+        className="sidebar__sendmail"
+        onClick={() => dispatch(openForward())}
+
+
+        
+      >
+     
+        <SidebarOption
+  
+        title="Forward"
+        Icon={ArrowForward}
+        
+        />
+        
+        
+      </Button>
+
+         
+      
+    </div>
+      
       <div className="mail__body">
         <div className="mail__bodyHeader">
           <h2>{selectedMail?.subject}</h2>
@@ -34,8 +93,15 @@ function Mail() {
         </div>
         <img width="600" src={selectedMail?.avatar} />
       </div>
+
+      
+      
     </div>
+    
   );
 }
+
+
+
 
 export default Mail;
