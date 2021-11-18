9d4c8c10ee3c183ebab0142f1a4977a9d07b8fa0
Cooper Riley <89549905+cooper350@users.noreply.github.com>
2021-11-18 09:07:55 -0600

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./Header.css";
-import MenuIcon from "@material-ui/icons/Menu";
-import { Avatar, IconButton } from "@material-ui/core";
-import SearchIcon from "@material-ui/icons/Search";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import { Avatar } from "@material-ui/core";
 import { useDispatch, useSelector } from "react-redux";
 import { logout, selectUser } from "./features/userSlice";
 import { auth } from "./firebase";
 import { db } from "./firebase";
-import EmailRow from "./EmailRow";
 
 function Header() {
   const [emails, setEmails] = useState([]);
@@ -46,27 +42,14 @@ function Header() {
   return (
     <div className="header">
       <div className="header__left">
-        <IconButton>
-          <MenuIcon />
-        </IconButton>
+       
       </div>
 
       <div className="header__middle">
-        <SearchIcon />
-        <input placeholder="Search mail" type="text" onChange={(e) => setSearch(e.target.value)}/>
-        <ArrowDropDownIcon className="header__inputCaret" />
+        E-Client
       </div>
-      <div className="">
-        {filteredContacts.map(({ id, data: { to, subject, message, timestamp } }) => (
-          <EmailRow
-            id={id}
-            key={id}
-            title={to}
-            subject={subject}
-            description={message}
-            time={new Date(timestamp?.seconds * 1000).toUTCString()}
-          />
-        ))}
+      <div className="flex">
+        
       </div>
       <div className="header__right">
         <Avatar onClick={signOut} src={user?.photoUrl} />
