bbea439ba0bcf854dc42ba242dedd88e5d8a20a6
Cooper Riley <89549905+cooper350@users.noreply.github.com>
2021-11-18 09:21:08 -0600

    Update Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,10 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 import "./Sidebar.css";
-import AddIcon from "@material-ui/icons/Add";
-import InboxIcon from "@material-ui/icons/Inbox";
 import SidebarOption from "./SidebarOption";
 import { useDispatch } from "react-redux";
 import { openSendMessage } from "./features/mailSlice";
+import { Send } from "@material-ui/icons";
 
 function Sidebar() {
   const dispatch = useDispatch();
@@ -16,15 +15,16 @@ function Sidebar() {
         className="sidebar__sendmail"
         onClick={() => dispatch(openSendMessage())}
       >
-        SendMail
+     
+        <SidebarOption
+        Icon={Send}
+        title="Send Mail"
+        selected={true}
+        />
       </Button>
 
-      <SidebarOption
-        Icon={InboxIcon}
-        title="Mail"
-        number={10}
-        selected={true}
-      />
+         
+      
     </div>
   );
 }
